6102a870ddc877e61f67daea0473a02f0daa8f87
tdfalc <tomdfalc@gmail.com>
2025-01-13 14:10:06 +0100

    Use subplot for congruent types

--- analytics/sim01_binary_types_congruent.py
+++ analytics/sim01_binary_types_congruent.py
@@ -54,13 +54,13 @@ if __name__ == "__main__":
     num_types = 10000
     prob_state0s = np.linspace(0, 1, num_types)
 
-    def _plot_results(phi):
+    fig, axs = plt.subplots(3, 1, figsize=(6, 6))
+
+    def _plot_results(phi, ax, xlabel=True):
 
         taus_critical_low, taus_critical_high = compute_critical_taus(
             type_low, type_high, prob_state0s, phi
         )
-
-        fig, ax = plt.subplots(figsize=(6, 3))
 
         ax.plot(prob_state0s, taus_critical_low, color="k", ls="solid")
         ax.plot(prob_state0s, taus_critical_high, color="k", ls="dashed")
@@ -93,27 +93,31 @@ if __name__ == "__main__":
             label=r"$(I^l, I^h) = (1, 1)$",
         )
 
-        prettify(ax=ax, legend=True)
-
         ax.set_ylim(top=1, bottom=0.01)
         ax.set_xlim(left=0, right=1)
-        ax.set_xlabel(r"Seller's Information ($v_s$)")
-        ax.set_ylabel(r"Threshold $\tau$ ($\tau^\star$)")
-
-        return fig, ax
+        ax.set_ylabel(r"Threshold $\tau$")
+        if xlabel:
+            ax.set_xlabel(r"Seller's Information ($v_s$)")
+        prettify(ax=ax, legend=True if i == 0 else False)
 
     # Save static figure for paper
-    phi = 0.9
-    fig, _ = _plot_results(phi)
+    phis = [0.36, 0.5, 0.56]
+    for i, phi in enumerate(phis):
+        ax = axs[i]
+        ax.set_title(f"$\\phi = {phi:.1f}$")
+        _plot_results(phi, ax, xlabel=True if i == len(phis) - 1 else False)
+
     fig.tight_layout()
-    fig.savefig(savedir / f"binary_types_congruent_phi{phi:.4f}.pdf", dpi=300)
+    fig.savefig(savedir / f"binary_types_congruent.pdf", dpi=300)
 
     # Make gif with pillow
     frames = []
     phis = np.concatenate((np.linspace(0.01, 0.49, 49), np.linspace(0.51, 0.65, 15)))
+
     for phi in phis:
-        fig, ax = _plot_results(phi)
-        ax.set_title(f"$\\phi = {phi:.3f}$")
+        fig, ax = plt.subplots(figsize=(6, 3))
+        _plot_results(phi, ax)
+        ax.set_title(f"$\\phi = {phi:.2f}$")
         fig.tight_layout()
 
         # Save as BytesIO object temporarily to avoid saving every frame to disc
