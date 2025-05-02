cae3d96ccf3fc3892f364e240afaa9a3d8716d50
tdfalc <tomdfalc@gmail.com>
2025-05-02 09:25:35 +0200

    Update README.md

--- README.md
+++ README.md
@@ -15,3 +15,22 @@ If you find this useful in your work, we kindly request that you cite the follow
 }
 ```
 
+In this work, we:
+
+* Model a competitive market as a game of incomplete information between two players.
+* Consider a setting where one player observes a payoff-relevant state and can sell (possibly noisy) messages about it to the other player.
+* Formulate the decision of what information to sell, and at what price, as a joint problem of [mechanism design](https://en.wikipedia.org/wiki/Mechanism_design) and [information design](https://en.wikipedia.org/wiki/Bayesian_persuasion).
+* Characterize the profit-maximizing mechanism, where profit is defined as the payment received minus the informational externality, i.e., the cost of refining a competitor’s beliefs.
+
+
+* Model a competitive market as game of incomplete information between two players.
+* Consider one player observes some payoff-relevant state and can sell (possibly noisy)
+messages thereof to the other.
+* Frame the decision of what information to sell, and at what price, as a joint problem of [mechanism design](https://en.wikipedia.org/wiki/Mechanism_design) and [information design](https://en.wikipedia.org/wiki/Bayesian_persuasion).
+* Characetize the mechanism that maximizes profit, which is  the payment minus the externality induced by selling information to a competitor, that is, the cost of refining a competitor’s beliefs.
+
+As an example, in the case of congruent binary types (Section 4.1), information is sold only to the low type when the intensity of competition ($\tau$) is below the solid line, and only to the high type when $\tau$ is below the dashed line (see Figure 6).
+
+<p align="center" width="100%">
+    <img  src="./analytics/docs/sim01_binary_types_congruent/binary_types_congruent.gif">
+</p>
--- analytics/sim00_value_of_experiments.py
+++ analytics/sim00_value_of_experiments.py
@@ -12,17 +12,6 @@ logger = create_logger(__name__)
 
 def calculate_exerpiment_value(informativeness: float, type: float) -> float:
     return np.maximum(
-        0,
-        type * informativeness
-        + 1
-        + np.minimum(-informativeness, 0)
-        - (1 - type) * (type <= 0.5)
-        - type * (type > 0.5),
-    )
-
-
-def calculate_exerpiment_value(informativeness: float, type: float) -> float:
-    return np.maximum(
         0, 1 - informativeness * ((informativeness >= 0) - type) - np.maximum(type, 1 - type)
     )
 
@@ -36,55 +25,21 @@ if __name__ == "__main__":
     savedir = Path(__file__).parent / "docs/sim00_value_of_experiments"
     os.makedirs(savedir, exist_ok=True)
 
-    # fig, ax = plt.subplots(figsize=(6.5, 3), dpi=300)
     fig, ax = plt.subplots(figsize=(4, 2.5))
 
     num_types = 1000
     types = np.linspace(0, 1, num_types)
-    allocations = np.linspace(-1, 1, num_types)
+    allocations = [-0.8, 0, 0.36]
+    labels = [r"$-4/5$", r"0", r"$2/5$"]
+    linestyles = ["dashdot", "solid", "dashed"]
     experiment_values = np.zeros((num_types, num_types))
 
     for i, allocation in enumerate(allocations):
-        experiment_values[i, :] = calculate_exerpiment_value(allocation, types)
-
-    ax.plot(types, experiment_values[500, :], color="k", lw=1, label=r"$0$")
-    ax.plot(types, experiment_values[680, :], color="k", ls="dashed", lw=1, label=r"$2/5$")
-    ax.plot(types, experiment_values[130, :], color="k", ls="dashdot", lw=1, label=r"$-4/5$")
-
-    print(allocations[500], allocations[680], allocations[100])
-    # for i, allocation in enumerate(allocations):
-    #     experiment_values[i, :] = calculate_exerpiment_value(allocation, types)
-
-    # ax.plot(types, experiment_values[500, :], color="k", lw=1)
-    # ax.plot(types, experiment_values[750, :], color="k", ls="dashed", lw=1)
-
-    # # Create a meshgrid
-    # X, Y = np.meshgrid(types * 6, allocations * 2)
-
-    # # Use pcolormesh instead of imshow for a gridded look
-    # # cmap = "Blues"
-    # # pc = ax.pcolormesh(X, Y, experiment_values, shading="auto", cmap=cmap)
-
-    # pc = ax.pcolormesh(X, Y, experiment_values, shading="auto", cmap="Blues")
-
-    # # Colorbar
-    # divider = make_axes_locatable(ax)
-    # cax = divider.append_axes("right", size="4%", pad=0.3)
-    # cbar = fig.colorbar(pc, cax=cax)
-    # cbar.set_label(r"$\delta (I, v_b)$", labelpad=10)
-    # # cbar = fig.colorbar(pc, ax=ax, orientation="vertical", pad=-0.50, aspect=40)
-    # # cbar.set_label(r"$\delta (I, v_b)$", labelpad=5)
+        experiment_values = calculate_exerpiment_value(allocation, types)
+        ax.plot(types, experiment_values, color="k", lw=1, label=labels[i], ls=linestyles[i])
 
     ax.set_xlabel("$v_b$")
-    # ax.set_ylabel(r"$I(v_b)$")
     ax.set_ylabel(r"$\delta(I, v_b)$")
-    ax.axvline(x=0.5, c="k", zorder=0, alpha=0.3, lw=1)
-    # ax.set_xticks((0, 1.5, 3, 4.5, 6))
-    # ax.set_xticklabels(("$0.0$", "$0.25$", "$0.5$", "$0.75$", "$1.0$"))
-    # ax.set_yticks((-2, -1, 0, 1, 2))
-    # ax.set_yticklabels(("$-1$", "$-0.5$", "$0$", "$0.5$", "$1$"))
-
     prettify(ax=ax, legend=True)
-
     fig.tight_layout()
     fig.savefig(savedir / "value_of_experiments.pdf", dpi=300)
